18842df0d7663bf7cccb5509561e4dd0da80e541
EliyaPelton <eepstudent@icloud.com>
2024-03-03 18:10:00 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Variable to specify which item should be displayed in the "item_display_section" of Sale & Purchase Pages
+let currentItem;
+
+//DROPDOWN FEATURE of "search_inventory_section" section of Sale & Purchase Pages
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
